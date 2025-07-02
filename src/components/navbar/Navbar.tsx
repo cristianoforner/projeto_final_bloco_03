4396634c04d32880df024763730253e968876da5
Alex Ikezili <alex.ikezili@icloud.com>
2025-07-02 09:35:43 -0300

    :sparkles: Criação componentes Home, Navbar, Footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,52 @@
+import { UserIcon, ShoppingCartIcon } from "@phosphor-icons/react"
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-gray-900 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                    <img className="w-[208px] h-[79.35]" src="logo.png"/>
+
+                    <div className="flex items-center flex-1 max-w-md mx-4 gap-2">
+                        <input
+                            type="text"
+                            placeholder="Procurar"
+                            className="flex-1 px-4 py-2 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+                        <button
+                            className="p-3 rounded-md bg-white hover:bg-gray-200"
+                        >
+                            <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5 text-black"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M21 21l-4.35-4.35M17 11a6 6 0 11-12 0 6 6 0 0112 0z"
+                            />
+                            </svg>
+                        </button>
+                        </div>
+
+                    <div className='flex gap-4 items-center'>
+                        <div className="flex gap-8 px-8 ">
+                        <p>Categorias</p>
+                        <p>Cadastrar Categoria</p>
+                        </div>
+                        <UserIcon size={36} weight="regular" />
+                        <ShoppingCartIcon size={36} weight="regular" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
